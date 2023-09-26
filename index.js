9357f4b6a19c93fa1701cc9d3fb8515d077fea18
Jayakanth <jayakanth1812@gmail.com>
2023-09-26 13:26:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var myName = "Jayakanth";
+// number array
+var numArray = [1, 3, 5, 7, 9];
+//string array
+var stringArray = ["milk", "bread", "cookies"];
+//tuples
+var userData = ["jayakanth", 22];
+console.log(userData[0]);
---
+++ index.ts
@@ -0,0 +1,14 @@
+const myName: string = "Jayakanth";
+
+//any
+let newData; //by default if the the variable is not initialized it asigned to any type.
+
+// number array
+const numArray: number[] = [1, 3, 5, 7, 9];
+//string array
+const stringArray: string[] = ["milk", "bread", "cookies"];
+
+//tuple
+const userData: [string, number] = ["jayakanth", 22];
+//named tuple
+const userData2: [name: string, age:number] = ["hari", 24]
